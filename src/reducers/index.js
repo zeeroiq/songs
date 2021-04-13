dd605036f516edc9eb6ca4c574527ac8ca1124ca
zeero1q <shrikantvishwakrma74@gmail.com>
2021-04-14 01:54:23 +0530

    adding reducers code. ref #2

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+    return [
+        { title: 'No Scrubs', duration: '4:05' },
+        { title: 'Sky fall', duration: '3:43' },
+        { title: 'Coldplay Hymn', duration: '4:46' },
+        { title: 'Hye Garami', duration: '5:00' }
+    ];
+};
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if(action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+};
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
