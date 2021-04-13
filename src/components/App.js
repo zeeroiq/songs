3e88f91760b11638d7835d436e44565cafb9fe0a
zeero1q <shrikantvishwakrma74@gmail.com>
2021-04-14 01:23:13 +0530

    Some modifications and other starters as well actions added. issue #1

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { selectSong } from '../actions';
+
+const App = () => {
+    return (
+        <div>
+            App
+        </div>
+    );
+}
+
+export default App;
